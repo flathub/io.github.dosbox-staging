d994ff7f7b10008a79a2a39909c573302606e67a
Robert de Rooy <17701098+rderooy@users.noreply.github.com>
2026-08-28 11:54:20 +0200

    prepare for 0.83 update

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 clear
-flatpak-builder --repo=testing-repo --force-clean build-dir io.github.dosbox-staging.yml
+flatpak-builder --repo=testing-repo --force-clean build-dir io.github.dosbox-staging.yml || exit 1
 flatpak --user remote-add --if-not-exists --no-gpg-verify dos-testing-repo testing-repo
 flatpak --user install dos-testing-repo io.github.dosbox-staging -y
 flatpak --user install dos-testing-repo io.github.dosbox-staging.Debug -y
